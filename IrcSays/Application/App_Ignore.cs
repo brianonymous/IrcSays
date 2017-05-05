61e48f8795740bd02c8aca0b8ca69f313f8eeb57
Brian Bosworth <bbosworth76@gmail.com>
2017-05-05 17:10:34 -0400

    "Fixed" ignore all on Join/Part/Quit msgs.

--- IrcSays/Application/App_Ignore.cs
+++ IrcSays/Application/App_Ignore.cs
@@ -1,119 +1,120 @@
-﻿using System.Collections.Generic;
+﻿using IrcSays.Communication.Irc;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using IrcSays.Communication.Irc;
 
 namespace IrcSays.Application
 {
-	public partial class App : System.Windows.Application
-	{
-		private class IgnoreInfo
-		{
-			private Regex _pattern;
+    public partial class App : System.Windows.Application
+    {
+        private static readonly Dictionary<string, IgnoreInfo> _ignores = new Dictionary<string, IgnoreInfo>();
 
-			public string Mask { get; private set; }
-			public IgnoreActions Actions { get; set; }
+        public static void AddIgnore(string mask, IgnoreActions actions)
+        {
+            var key = mask.ToUpperInvariant();
+            if (!_ignores.ContainsKey(key))
+            {
+                _ignores.Add(key, new IgnoreInfo(mask));
+            }
+            _ignores[key].Actions |= actions;
+            SaveIgnores();
+        }
 
-			public IgnoreInfo(string mask)
-			{
-				Mask = mask;
-			}
+        public static IEnumerable<string> GetIgnoreInfo()
+        {
+            foreach (var i in _ignores.Values)
+            {
+                yield return string.Format("{0} {1}", i.Mask, i.Actions);
+            }
+        }
 
-			public bool IsMatch(string prefix, IgnoreActions action)
-			{
-				if (_pattern == null)
-				{
-					_pattern = new Regex("^" + Regex.Escape(Mask).Replace("\\*", ".*").Replace("\\?", ".") + "$",
-						RegexOptions.IgnoreCase);
-				}
-				return (Actions & action) != IgnoreActions.None && _pattern.IsMatch(prefix);
-			}
-		}
+        public static bool IsIgnoreMatch(IrcPrefix prefix, IgnoreActions action)
+        {
+            switch (action)
+            {
+                case IgnoreActions.Join:
+                case IgnoreActions.Part:
+                case IgnoreActions.Quit:
+                    return false;
+            }
 
-		private static readonly Dictionary<string, IgnoreInfo> _ignores = new Dictionary<string, IgnoreInfo>();
+            if (prefix == null)
+            {
+                return false;
+            }
 
-		public static IEnumerable<string> GetIgnoreInfo()
-		{
-			foreach (var i in _ignores.Values)
-			{
-				yield return string.Format("{0} {1}", i.Mask, i.Actions);
-			}
-		}
+            return (from ignore in _ignores.Values
+                    where ignore.IsMatch(prefix.Prefix, action)
+                    select true).Any();
+        }
 
-		public static void AddIgnore(string mask, IgnoreActions actions)
-		{
-			var key = mask.ToUpperInvariant();
-			if (!_ignores.ContainsKey(key))
-			{
-				_ignores.Add(key, new IgnoreInfo(mask));
-			}
-			_ignores[key].Actions |= actions;
-			SaveIgnores();
-		}
+        public static bool RemoveIgnore(string mask, IgnoreActions actions)
+        {
+            var key = mask.ToUpperInvariant();
+            if (!_ignores.ContainsKey(key) ||
+                (_ignores[key].Actions & actions) == 0)
+            {
+                return false;
+            }
 
-		public static bool RemoveIgnore(string mask, IgnoreActions actions)
-		{
-			var key = mask.ToUpperInvariant();
-			if (!_ignores.ContainsKey(key) ||
-				(_ignores[key].Actions & actions) == 0)
-			{
-				return false;
-			}
+            _ignores[key].Actions &= ~actions;
+            if (_ignores[key].Actions == IgnoreActions.None)
+            {
+                _ignores.Remove(key);
+            }
+            SaveIgnores();
+            return true;
+        }
 
-			_ignores[key].Actions &= ~actions;
-			if (_ignores[key].Actions == IgnoreActions.None)
-			{
-				_ignores.Remove(key);
-			}
-			SaveIgnores();
-			return true;
-		}
+        private static void LoadIgnoreMasks()
+        {
+            _ignores.Clear();
+            foreach (var i in Settings.Current.Ignore.Split(','))
+            {
+                var parts = i.Split(' ');
+                if (i.Trim().Length < 1)
+                {
+                    continue;
+                }
+                var mask = parts[0];
+                int iactions;
+                if (!(parts.Length > 1 && int.TryParse(parts[1], out iactions)))
+                {
+                    iactions = (int)IgnoreActions.All;
+                }
+                _ignores.Add(mask.ToUpperInvariant(), new IgnoreInfo(mask) { Actions = (IgnoreActions)iactions });
+            }
+        }
 
-		public static bool IsIgnoreMatch(IrcPrefix prefix, IgnoreActions action)
-		{
-			switch (action)
-			{
-				case IgnoreActions.Join:
-				case IgnoreActions.Part:
-				case IgnoreActions.Quit:
-					return true;
-			}
+        private static void SaveIgnores()
+        {
+            Settings.Current.Ignore = string.Join(",",
+                from i in _ignores.Values
+                select string.Format("{0} {1}", i.Mask, (int)i.Actions));
+        }
 
-			if (prefix == null)
-			{
-				return false;
-			}
+        private class IgnoreInfo
+        {
+            private Regex _pattern;
 
-			return (from ignore in _ignores.Values
-				where ignore.IsMatch(prefix.Prefix, action)
-				select true).Any();
-		}
+            public IgnoreInfo(string mask)
+            {
+                Mask = mask;
+            }
 
-		private static void LoadIgnoreMasks()
-		{
-			_ignores.Clear();
-			foreach (var i in Settings.Current.Ignore.Split(','))
-			{
-				var parts = i.Split(' ');
-				if (i.Trim().Length < 1)
-				{
-					continue;
-				}
-				var mask = parts[0];
-				int iactions;
-				if (!(parts.Length > 1 && int.TryParse(parts[1], out iactions)))
-				{
-					iactions = (int) IgnoreActions.All;
-				}
-				_ignores.Add(mask.ToUpperInvariant(), new IgnoreInfo(mask) {Actions = (IgnoreActions) iactions});
-			}
-		}
+            public IgnoreActions Actions { get; set; }
 
-		private static void SaveIgnores()
-		{
-			Settings.Current.Ignore = string.Join(",",
-				from i in _ignores.Values
-				select string.Format("{0} {1}", i.Mask, (int) i.Actions));
-		}
-	}
+            public string Mask { get; }
+
+            public bool IsMatch(string prefix, IgnoreActions action)
+            {
+                if (_pattern == null)
+                {
+                    _pattern = new Regex("^" + Regex.Escape(Mask).Replace("\\*", ".*").Replace("\\?", ".") + "$",
+                        RegexOptions.IgnoreCase);
+                }
+                return (Actions & action) != IgnoreActions.None && _pattern.IsMatch(prefix);
+            }
+        }
+    }
 }
